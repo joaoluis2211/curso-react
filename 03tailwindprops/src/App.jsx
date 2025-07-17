6cd385fd1f1c51469a2648d6d7f90c1dc2f2c830
JOAO <joaolsilva2211.com>
2025-07-17 16:03:30 -0300

    bgChangeColor

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <h1 className='text-black text-3xl'>Vite com tailwind</h1>
+      <Card />
     </>
   )
 }
